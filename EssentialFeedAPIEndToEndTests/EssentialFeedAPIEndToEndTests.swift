747ca230a6684c483b1b2ab18b28e4993660e0b1
TrabelsiAchraf <achraf.trabelsi@engie.com>
2021-11-19 20:27:39 +0100

    Replace custom 'LoadFeedResult' enum with the standard 'Swift.Result'

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -48,7 +48,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 10.0)
+        wait(for: [exp], timeout: 15.0)
         
         return receivedResult
     }
